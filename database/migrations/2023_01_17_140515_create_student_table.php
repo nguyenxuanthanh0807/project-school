18023b45c1904b8a86b3e3c4648e2700e1f366d3
nguyenxuanthanh0807 <nguyenxuanthanh0807@gmail.com>
2023-01-18 23:17:45 +0700

    update

---
+++ database/migrations/2023_01_17_140515_create_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')-> unique();
+            $table->string('password');
+            $table->integer('active')->nullable(); 
+            $table->string('gender')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
---
+++ database/migrations/2023_01_17_142523_create_account_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->id('account_id');
+            $table->string('email')-> unique();
+            $table->string('password');
+            $table->string('fullname');
+            $table->integer('role')->nullable(); 
+            $table->integer('active')->nullable(); 
+            $table->string('image')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account');
+    }
+};
